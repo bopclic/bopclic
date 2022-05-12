f97d85a8ec8bd6efe2d0671a3ab39afea40d0467
bopclic <83652229+bopclic@users.noreply.github.com>
2022-05-12 05:05:34 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -194,3 +194,20 @@ anime.timeline({ loop: true })
         delay: 1000
     });
 
+// Wrap every letter in a span
+var textWrapped = document.querySelector('.ml10 .letters');
+textWrapped.innerHTML = textWrapped.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({ loop: true })
+    .add({
+        targets: '.ml10 .letter',
+        rotateY: [-90, 0],
+        duration: 1300,
+        delay: (el, i) => 45 * i
+    }).add({
+        targets: '.ml10',
+        opacity: 0,
+        duration: 1000,
+        easing: "easeOutExpo",
+        delay: 1000
+    });
